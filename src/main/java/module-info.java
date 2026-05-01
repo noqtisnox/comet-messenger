890ad06089e92ba4dc002a94bf5952a5804d36ec
nox <cheporte.dev@gmail.com>
2026-05-01 21:51:15 +0300

    chore: updated exports in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,13 +11,16 @@ module com.comet {
     requires jbcrypt;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jsr310;
-    requires org.java_websocket;
+    requires transitive org.java_websocket;
 
     opens com.comet.ui.controller to javafx.fxml;
 
+    exports com.comet.model;
+    exports com.comet.config;
+    exports com.comet.service;
+    exports com.comet.repository;
+    exports com.comet.model.enums;
     exports com.comet.ui.controller;
     exports com.comet.network.client;
     exports com.comet.network.server;
-    exports com.comet;
-    exports com.comet.config;
 }
